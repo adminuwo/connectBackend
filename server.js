740a84bb7431541b47b8cbf8ff7da58f37a6fd76
aditi-050804 <aditi@uwo24.com>
2026-05-05 17:44:48 +0530

    final: switch to full node image and defensive env handling

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
     credentials: true
 }));
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
