c4bfd0e193736f3b8f0bca07137884ed12b3aee3
aditi-050804 <aditi@uwo24.com>
2026-05-11 14:13:08 +0530

    BBOT

--- server.js
+++ server.js
@@ -719,7 +719,7 @@ app.post('/webhook/interakt/:clientId', async (req, res) => {
                                 fullPhoneNumber: customerPhone,
                                 type: 'Text',
                                 data: { message: response }
-                            }, { headers: { 'Authorization': `Basic ${authKey}` } });
+                            }, { headers: { 'Authorization': `Developer ${authKey}` } });
                         }
 
                         // 2. Send Image if generated
@@ -732,7 +732,7 @@ app.post('/webhook/interakt/:clientId', async (req, res) => {
                                     originalUrl: imageUrl,
                                     previewUrl: imageUrl
                                 }
-                            }, { headers: { 'Authorization': `Basic ${authKey}` } });
+                            }, { headers: { 'Authorization': `Developer ${authKey}` } });
                         }
 
                         // 3. Save everything to DB
